7b63a22e0a77d74c2fb757cb5bb5f96f785af6d8
Victor Jackson <victorj.jackson@gmail.com>
2016-01-24 22:48:37 -0800

    updated access rights for on pictures

--- public/javascript/services/PinService.js
+++ public/javascript/services/PinService.js
@@ -6,10 +6,21 @@ var app;
         var PinService = (function () {
             function PinService($resource) {
                 this.$resource = $resource;
-                this.pinResource = $resource("/api/pins/:id");
+                this.pinResource = $resource("/api/pins/:id", null, {
+                    "update": { method: "PUT" }
+                });
             }
+            PinService.prototype.getAll = function () {
+                return this.pinResource.query();
+            };
+            PinService.prototype.getPin = function (pinId) {
+                return this.pinResource.get({ id: pinId });
+            };
             PinService.prototype.savePin = function (pin) {
                 return this.pinResource.save(pin).$promise;
+            };
+            PinService.prototype.updatePin = function (pin) {
+                return this.pinResource.update({ id: pin._id }).$promise;
             };
             PinService.prototype.deletePin = function (pin) {
                 return this.pinResource.delete({ id: pin._id }).$promise;
--- public/javascript/services/PinService.ts
+++ public/javascript/services/PinService.ts
@@ -3,8 +3,17 @@ namespace app.Services {
     export class PinService {
         private pinResource;
 
+        public getAll() {
+            return this.pinResource.query()
+        }
+        public getPin(pinId)    {
+            return this.pinResource.get({ id: pinId });
+        }
         public savePin(pin) {
             return this.pinResource.save(pin).$promise;
+        }
+        public updatePin(pin)   {
+            return this.pinResource.update({ id: pin._id }).$promise;
         }
         public deletePin(pin)   {
             return this.pinResource.delete({ id: pin._id }).$promise;
@@ -13,7 +22,9 @@ namespace app.Services {
         constructor(
             private $resource: ng.resource.IResourceService
         ){
-            this.pinResource = $resource("/api/pins/:id");
+            this.pinResource = $resource("/api/pins/:id", null, {
+                "update": {method: "PUT"}
+            });
         }
     }
     angular.module("app").service("PinService", PinService);
